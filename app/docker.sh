9af773630fdbb397e59b2a7ca9778acb17af77cf
VynoDePal <83235998+VynoDePal@users.noreply.github.com>
2024-05-18 17:21:58 +0100

    feat: fix bug of docker

--- app/docker.sh
+++ app/docker.sh
@@ -1,4 +1,4 @@
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console doctrine:migrations:diff --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
-Records Management Project
+exec apache2-foreground
