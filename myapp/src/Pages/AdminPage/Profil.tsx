0efae0aca6d3f17b6909dca99715a1ba30156265
VynoDePal <odjokevyn@gmail.com>
2024-05-26 17:17:48 +0100

    style: refonte design profilof user

--- myapp/src/Pages/AdminPage/Profil.tsx
+++ myapp/src/Pages/AdminPage/Profil.tsx
@@ -14,8 +14,16 @@ const Profil: React.FC = () => {
 
   useEffect(() => {
     const fetchUser = async () => {
+      const authUser = localStorage.getItem('authUser');
       try {
-        const response = await axios.get("https://localhost:8000/api/api/users");
+        if (!authUser) {
+          throw new Error("User not authenticated");
+        }
+        console.log(authUser);
+        const headers = {
+          Authorization: `Bearer ${authUser}`,
+        };
+        const response = await axios.get("https://localhost:8000/api/api/users", { headers });
         setUser(response.data);
         console.log(response.data);
       } catch (error) {
@@ -30,42 +38,45 @@ const Profil: React.FC = () => {
   });
 
   if (!user) {
-    return <div>Loading...</div>;
+    return <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-blue-500"></div>
+    </div>;
   }
 
   return (
-    <div
-      className="bg-black justify-center items-center dark:bg-black text-black dark:text-white p-6 rounded-lg shadow-md absolute"
-      style={{
-        width: "500px",
-        height: "500px",
-        marginLeft: "500px",
-        marginTop: "200px",
-      }}
-    >
-      <img
-        src={"default-profile-image-url"}
-        alt={` profile`}
-        className="rounded-full w-32 h-32 object-cover mx-auto mb-4 border-black border-2"
-      />
-      <h2 className="text-xl font-semibold mb-4">
-        Informations de l'utilisateur
-      </h2>
-      <div>
-        <strong>Nom:</strong> {user.firstname}
-        <br />
-        <strong>Email:</strong> {user.email}
-        <br />
-        <strong>Identifiant:</strong> {user.login}
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-800">
+      <div className="flex items-center justify-center relative w-1/2">
+        <img
+          src={"default-profile-image-url"}
+          // alt={`profile`}
+          className="rounded-full w-32 h-32 object-cover mx-auto border-2 border-black"
+        />
+        <div className="absolute -top-16 left-1/2 transform translate-x-[-50%] text-white text-2xl">
+          Profil
+        </div>
       </div>
-      <p className="mt-4">
-        <strong>Mot de passe:</strong> (Cliquez ici pour modifier)
-      </p>
-      <button className="mt-20 ml-20 bg-black hover:bg-gray-900 text-white font-bold py-2 px-4 rounded">
-        Modifier les informations
-      </button>
+      <div className="flex flex-col items-center justify-center w-1/2 mt-8">
+        <h2 className="text-3xl font-semibold">Informations de l'utilisateur</h2>
+        <div className="flex flex-col items-center justify-center w-full mt-4">
+          <p className="text-lg">
+            <strong>Nom:</strong> {user.firstname}
+          </p>
+          <p className="text-lg">
+            <strong>Email:</strong> {user.email}
+          </p>
+          <p className="text-lg">
+            <strong>Identifiant:</strong> {user.login}
+          </p>
+        </div>
+        <div className="mt-6">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            Modifier les informations
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Profil;
+
